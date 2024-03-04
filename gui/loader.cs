035086f4db34cc46eb66df2a7fa1b6f50facdd64
emmy <52672675+emmaccode@users.noreply.github.com>
2024-03-04 01:17:38 -0700

    more maps + gui

---
+++ gui/loader.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+// The goal of the loader is to handle everything from players and their inputs
+// to map-loading, and controls :)
+public partial class loader : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
